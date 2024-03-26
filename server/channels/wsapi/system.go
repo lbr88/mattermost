7e777df8151ef756c10a4edf9436439a5ea72b1f
Devin Binnie <devin.binnie@mattermost.com>
2024-03-26 16:05:43 -0400

    Basic websocket POSTED event ACK

--- server/channels/wsapi/system.go
+++ server/channels/wsapi/system.go
@@ -5,10 +5,13 @@ package wsapi
 
 import (
 	"github.com/mattermost/mattermost/server/public/model"
+	"github.com/mattermost/mattermost/server/public/shared/mlog"
 )
 
 func (api *API) InitSystem() {
 	api.Router.Handle("ping", api.APIWebSocketHandler(ping))
+	api.Router.Handle("websocket_posted_ack", api.APIWebSocketHandler(api.websocketNotificationAck))
+
 }
 
 func ping(req *model.WebSocketRequest) (map[string]any, *model.AppError) {
@@ -19,4 +22,19 @@ func ping(req *model.WebSocketRequest) (map[string]any, *model.AppError) {
 	data["node_id"] = ""
 
 	return data, nil
+}
+
+func (api *API) websocketNotificationAck(req *model.WebSocketRequest) (map[string]any, *model.AppError) {
+	// Log if the websocket event resulted in a notification
+	api.App.NotificationsLog().Trace("Websocket notification acknowledgment",
+		mlog.String("type", model.TypeWebsocket),
+		mlog.String("user_id", req.Session.UserId),
+		mlog.Any("user_agent", req.Data["user_agent"]),
+		mlog.Any("post_id", req.Data["post_id"]),
+		mlog.Any("result", req.Data["result"]),
+		mlog.Any("reason", req.Data["reason"]),
+		mlog.Any("data", req.Data["data"]),
+	)
+
+	return nil, nil
 }
